7a2ee5bf29f5d16e4c9e1de1df9a68ea74109c6a
Masrafi404 <mdmasrafi902@gmail.com>
2025-06-02 06:21:23 +0600

    string iterators

---
+++ string_iterators.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Hello World";
+
+    // cout << *s.begin() << " " << *(s.end() - 1) << endl;
+
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     cout << s[i] << endl;
+    // }
+
+    // for (string::iterator it = s.begin(); it < s.end(); it++)
+    // {
+    //     cout << *it << endl;
+    // }
+
+    for (auto it = s.begin(); it < s.end(); it++)
+    {
+        cout << *it << endl;
+    }
+
+    return 0;
+}
